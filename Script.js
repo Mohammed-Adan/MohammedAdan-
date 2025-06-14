b3f7a925635ef7ecc849ba1dd0a8349962287224
Mohammed Adan <mohammedadanmohammud82@gmail.com>
2025-06-14 12:14:13 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -386,7 +386,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const articles = [
 
   {
-    id: 'Halista-Airpodska',
+    id: 'Halista Airpodska',
     title:'ISTECMAALKA WAQTIGA BADAN EE AIRPODS-KA ',
     excerpt:'In kastoo AirPods-ku ay raaxo yihiin, haddana isticmaalka muddada dheer ee mugga sare..',
     date:'June 14,2025'
